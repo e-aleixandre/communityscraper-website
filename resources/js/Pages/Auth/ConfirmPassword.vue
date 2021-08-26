ed6e1951b2f7a591e964816715b1842657f7049c
EAleixandre <ealeixandredev@gmail.com>
2021-08-26 14:57:30 +0200

    Translated several pages to Spanish

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -2,20 +2,20 @@
     <Head title="Confirm Password" />
 
     <div class="mb-4 text-sm text-gray-600">
-        This is a secure area of the application. Please confirm your password before continuing.
+        Esta es una zona protegida de la aplicación. Por favor, confirma tu contraseña para continuar.
     </div>
 
     <BreezeValidationErrors class="mb-4" />
 
     <form @submit.prevent="submit">
         <div>
-            <BreezeLabel for="password" value="Password" />
+            <BreezeLabel for="password" value="Contraseña" />
             <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" autofocus />
         </div>
 
         <div class="flex justify-end mt-4">
             <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Confirm
+                Confirmar
             </BreezeButton>
         </div>
     </form>
--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -2,7 +2,7 @@
     <Head title="Forgot Password" />
 
     <div class="mb-4 text-sm text-gray-600">
-        Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+        ¿Olvidaste tu contraseña? No pasa nada. Introduce tu email y te enviaremos un enlace para reestablecer tu contraseña.
     </div>
 
     <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -19,7 +19,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <BreezeButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Email Password Reset Link
+                Enviar enlace
             </BreezeButton>
         </div>
     </form>

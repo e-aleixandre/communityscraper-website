6789f7257746c616b7c81ebeeb59dc757ee5a0a1
E. Aleixandre <ealeixandremp@gmail.com>
2021-08-27 14:56:25 +0200

    Applied laravel-ide-helper, great

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,9 +6,39 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property string filename
- * @property string token
- * @property mixed id
+ * App\Models\Report
+ *
+ * @property int $id
+ * @property string|null $filename
+ * @property string $min_date
+ * @property string $max_date
+ * @property float $progress
+ * @property int $completed
+ * @property int $errored
+ * @property string|null $token
+ * @property int $pid
+ * @property int $notify
+ * @property int|null $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereCompleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereErrored($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereFilename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereMaxDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereMinDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereNotify($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report wherePid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereProgress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereUserId($value)
+ * @mixin \Eloquent
  */
 class Report extends Model
 {
